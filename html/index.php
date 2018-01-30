9d6968523f8c94bbb6b98cb4ebf5822f0e8d06dd
CHIH-YANG HUANG <user@hcy.idv.tw>
2018-01-30 23:09:54 +0800

    set

--- html/index.php
+++ html/index.php
@@ -6,13 +6,13 @@
 <body>
 <form action="./afterquery.php" method="post">
   Start Date:<br>
-  <input type="date" name="sd"><br>
+  <input type="date" name="sd" required><br>
   End Date:<br>
-  <input type="date" name="ed"><br>
+  <input type="date" name="ed" required><br>
   Start Overall Quality:<br>
-  <input type="text" name="so"><br>
+  <input type="text" name="so" required><br>
   End Overall Quality:<br>
-  <input type="text" name="eo">
+  <input type="text" name="eo" required>
   <br>
   <INPUT TYPE=submit value="送出查詢">
 </form>

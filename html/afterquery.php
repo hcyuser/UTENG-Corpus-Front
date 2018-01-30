085ddd4647df4e514ecc6ed31620bd397607107b
CHIH-YANG HUANG <user@hcy.idv.tw>
2018-01-30 23:01:01 +0800

    Set

--- html/afterquery.php
+++ html/afterquery.php
@@ -35,7 +35,7 @@ echo "Query Result: ".$br;
 if ($result = $mysqli->query($sql)) {
     /* fetch associative array */
     while ($row = $result->fetch_assoc()) {
-        echo $row . $br ;
+        echo $row->id . $br ;
     }
     $result->free();
 }

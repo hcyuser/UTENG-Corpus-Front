85519ea48dc43466dba1421dbb905902a4083800
CHIH-YANG HUANG <user@hcy.idv.tw>
2018-01-30 22:26:47 +0800

    Add html form

--- html/index.php
+++ html/index.php
@@ -1,32 +1,19 @@
-<?php
-$host = "104.199.250.176";
-$user = "hcyidvtw";
-$password = "hcyidvtw";
-$database = "rating";
-$mysqli = new mysqli($host, $user, $password, $database);
+<HTML>
+<HEAD>
+<title>Rate My professor Corpus System</title>
+</HEAD>
 
-$br = "<br/>";
-
-/* check connection */
-if ($mysqli->connect_errno) {
-    echo "Error: Unable to connecct to MySQL.<br/>";
-    echo "Debugging errno: " . $mysqli->connect_errno() . $br;
-    echo "Debugging error: " . $mysqli->connect_error() . $br;
-    exit;
-}
-//echo "Success: A proper connection to MySQL was made! The $database database is great." . $br;
-//echo "Host information: " . $mysqli->host_info . $br;
-
-$sql = "SELECT * FROM professor WHERE id = 1234";
-echo "\$mysqli -> query(\"$sql\")" . $br;
-echo "Query Result: ".$br;
-if ($result = $mysqli->query($sql)) {
-    /* fetch associative array */
-    while ($row = $result->fetch_assoc()) {
-        echo json_encode($row);
-    }
-    $result->free();
-}
-
-mysqli_close($link);
-?>
+<body>
+<form action="./afterquery.php" method="post">
+  Start Date:<br>
+  <input type="text" name="sd"><br>
+  End Date:<br>
+  <input type="text" name="ed">
+  Start Overall Quality:<br>
+  <input type="text" name="so"><br>
+  End Overall Quality:<br>
+  <input type="text" name="eo">
+  <INPUT TYPE=submit value="送出查詢">
+</form>
+</body>
+</HTML>

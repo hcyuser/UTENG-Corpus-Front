539a6fbc1c50951d390cddd653a80c9879ded521
CHIH-YANG HUANG <user@hcy.idv.tw>
2018-02-11 16:36:35 +0800

    Set

---
+++ html/unisearch/index.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD>
+<title>Rate My professor Corpus System</title>
+</HEAD>
+
+<body>
+<form action="./unisearch.php" method="post">
+  Start DateYYYY-MM-DD:<br>
+  <input type="text" name="sd" value="2000-01-01" required><br>
+  End Date YYYY-MM-DD:<br>
+  <input type="text" name="ed" value="2017-01-01" required><br>
+  Start Overall Quality:<br>
+  <input type="text" name="so" value="3" required><br>
+  End Overall Quality:<br>
+  <input type="text" name="eo" value="5" required>
+  <br>
+  <INPUT TYPE=submit value="送出查詢">
+</form>
+</body>
+</HTML>
---
+++ html/unisearch/unisearch.php
@@ -0,0 +1,62 @@
+<?php
+$host = "104.199.250.176";
+$user = "hcyidvtw";
+$password = "hcyidvtw";
+$database = "rating";
+$mysqli = new mysqli($host, $user, $password, $database);
+
+$br = "<br/>";
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    echo "Error: Unable to connecct to MySQL.<br/>";
+    echo "Debugging errno: " . $mysqli->errno . $br;
+    echo "Debugging error: " . $mysqli->error . $br;
+    exit;
+}
+
+//echo "Success: A proper connection to MySQL was made! The $database database is great." . $br;
+//echo "Host information: " . $mysqli->host_info . $br;
+?>
+
+
+<?php
+//print_r($_POST);
+  if($_POST["sd"] && $_POST["ed"] && $_POST["so"] && $_POST["eo"]){
+    $sd = $mysqli->escape_string($_POST["sd"]);
+    $ed =  $mysqli->escape_string($_POST["ed"]);
+    $so =  $mysqli->escape_string($_POST["so"]);
+    $eo =  $mysqli->escape_string($_POST["eo"]);
+
+  }
+$sql = "SELECT * FROM response WHERE quality >= $so AND quality <= $eo AND STR_TO_DATE(date, '%m/%d/%Y') >= '$sd' AND STR_TO_DATE(date, '%m/%d/%Y')<= '$ed' LIMIT 10 ";
+//echo "\$mysqli -> query(\"$sql\")" . $br;
+//$escape = $mysqli->escape_string($sql);
+//echo $escape;
+echo "Query Result: ".$br;
+if ($result = mysqli_query($mysqli,$sql)) {
+    //print_r($result);
+    /* fetch associative array */
+    $randtxt = rand();
+    $myfile = fopen("./txtoutput/".$randtxt.".txt", "w") or die("Unable to open file!");
+    while ($row = $result->fetch_assoc()) {
+        $show = $row['id'].":".$row['tid'].":".$row['content'];
+        fwrite($myfile, $show."\r\n");
+        echo $show.$br;
+    }
+    fclose($myfile);
+    echo "http://uteng.hcy.idv.tw/txtoutput/".$randtxt.".txt";
+    $result->free();
+}else{
+  //print_r($result);
+  echo "Error: Unable to connecct to MySQL.<br/>";
+  echo "Debugging errno: " . $mysqli->errno . $br;
+  echo "Debugging error: " . $mysqli->error . $br;
+  exit;
+}
+?>
+
+
+<?php
+  $mysqli->close();
+?>

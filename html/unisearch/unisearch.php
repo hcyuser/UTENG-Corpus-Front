2eba45cf8dd320d94fd80f0b60c96df77d558a79
CHIH-YANG HUANG <user@hcy.idv.tw>
2018-02-11 17:29:02 +0800

    set

--- html/unisearch/unisearch.php
+++ html/unisearch/unisearch.php
@@ -29,7 +29,7 @@ if ($mysqli->connect_errno) {
     $eq =  $mysqli->escape_string($_POST["eq"]);
     $school =  $mysqli->escape_string($_POST["school"]);
   }
-$sql = "SELECT * FROM professor WHERE quality >= $sq AND quality <= $eq AND school like '$school' LIMIT 10";
+$sql = "SELECT id FROM professor WHERE quality >= $sq AND quality <= $eq AND school like '$school' LIMIT 10";
 //echo "\$mysqli -> query(\"$sql\")" . $br;
 //$escape = $mysqli->escape_string($sql);
 //echo $escape;

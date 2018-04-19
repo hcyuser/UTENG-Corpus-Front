7f5aeda0d48a05e5cfd663f563eb82a563f38f46
CHIH-YANG HUANG <user@hcy.idv.tw>
2018-04-19 16:25:24 +0800

    With random filename output

--- html/querywithquality/afterquery.php
+++ html/querywithquality/afterquery.php
@@ -18,10 +18,12 @@
       $eo = trim($_POST["eo"]);
       $school = trim($_POST["school"]);
     }
-    if(file_exists("../txtoutput/".$school."-".$so.".txt")){
+    /*if(file_exists("../txtoutput/".$school."-".$so.".txt")){
             unlink("../txtoutput/".$school."-".$so.".txt");
     }
-    $myfile = fopen("../txtoutput/".$school."-".$so.".txt", "w") or die("Unable to open file!");
+    $myfile = fopen("../txtoutput/".$school."-".$so.".txt", "w") or die("Unable to open file!"); */
+    $random = rand();
+    $myfile = fopen("./txtoutput/".$random.".txt", "w") or die("Unable to open file!");
     $rs=read_multi_record($__db, "select R.ID, R.TID, SUBBLOBTOCHAR(R.CONTENT,null,null), R.\"DATE\", R.QUALITY, R.Difficulty, R.C_date from user.response R inner join user.professor TC on R.tid=TC.id AND TC.quality >= '$so' AND TC.quality <= '$eo' AND TC.school like '$school' where R.C_DATE >= '$sd' AND R.C_DATE <= '$ed' AND  R.quality >= '$so' AND R.quality <= '$eo' ",array(),array());
     if ($rs === false){
       echo "101, \"".kwcr2_geterrormsg($__db, 1)."\"";
@@ -41,7 +43,7 @@
     fclose($myfile);
     echo "Query Result:".$br;
     echo "<html><a href=\"";
-    echo "http://uteng.hcy.idv.tw/txtoutput/".$school."-".$so.".txt";
+    echo "http://tcu.cyberhood.net/UT_ENG_Front/html/txtoutput/".$random.".txt";
     echo "\">Download Here</a></html>";
     kwcr2_unmapdb($__db);
 ?>
